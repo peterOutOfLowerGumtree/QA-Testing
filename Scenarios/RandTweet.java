c8b9793769d05f7179a0c5e79a27d915e5e3f947
peterOutOfLowerGumtree <jwhiteley0@icloud.com>
2017-05-26 10:56:19 +0100

    Add files via upload

---
+++ Scenarios/RandTweet.java
@@ -0,0 +1,62 @@
+package Files;
+
+public class RandTweet {
+	public String randTweet(int numb) {
+		String words;
+		switch (numb) {
+		case 1:
+			words = "It is no measure of health to be well adjusted to a profoundly sick society - Jiddu Krishnamurti";
+			break;
+		case 2:
+			words = "The only true happiness comes in death.";
+			break;
+		case 3:
+			words = "Your infatuation is based on a physical attraction. Talk to a woman and you'll realise you have nothing in common.";
+			break;
+		case 4:
+			words = "To live is to suffer.";
+			break;
+		case 5:
+			words = "The bees are dying. This is humankind's fault.";
+			break;
+		case 6:
+			words = "I can't even tolerate humans.";
+			break;
+		case 7:
+			words = "The heaviest object in the universe is the burden of existence in an eternally-recurring world.";
+			break;
+		case 8:
+			words = "Life: noun - a sexually transmitted, terminal disease.";
+			break;
+		case 9:
+			words = "Most people die a thousand spiritual deaths before the arbitrary corporeal one happens.";
+			break;
+		case 10:
+			words = "Becoming aware of late capitalism's ideological injunction to enjoy has led me to doubt my own enjoyment.";
+			break;
+		case 11:
+			words = "I am finding it hard to distinguish between authentically enjoying activities and enjoying them as a form of society's dominant ideology.";
+			break;
+		case 12:
+			words = "Every second is a second closer to the void.";
+			break;
+		case 13:
+			words = "If you work hard enough you can replace depression with exhaustion.";
+			break;
+		case 14:
+			words = "I don't know what is worse - that this tweet will outlive me or that I might outlive Twitter.";
+			break;
+		case 15:
+			words = "You are matter, but you don't matter.";
+			break;
+		case 16:
+			words = "Trade pieces of paper with people until you die. Humans are pointless.";
+			break;
+		default:
+			words = "NAN";
+			break;
+		}
+
+		return words;
+	}
+}
